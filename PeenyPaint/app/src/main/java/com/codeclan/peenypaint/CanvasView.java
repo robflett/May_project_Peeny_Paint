afcf81038eae2eaf404b6ff780182ebaea8f53cb
Rob <robflettcc@gmail.com>
2017-05-13 11:23:34 +0100

    Logic for line drawing added

--- PeenyPaint/app/src/main/java/com/codeclan/peenypaint/CanvasView.java
+++ PeenyPaint/app/src/main/java/com/codeclan/peenypaint/CanvasView.java
@@ -1,8 +1,70 @@
 package com.codeclan.peenypaint;
 
-/**
- * Created by user on 13/05/2017.
- */
+//variables begin with 'm' as they are non-public and non-static
 
-public class CanvasView {
+import android.content.Context;
+//import android.support.annotation.Nullable;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CanvasView extends View{
+
+    public int width;
+    public int height;
+
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    private Paint mPaint;
+
+    private float mX, mY;
+
+    Context context;
+
+//    the below compares floats - if the tolerance is below the given amount, they are considered equal
+    private static final float TOLERANCE = 5;
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+//        below we set 'this' to equal the above context
+        this.context = context;
+
+        mPath = new Path();
+
+        mPaint = new Paint();
+
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.MAGENTA);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(5f);
+
+    }
+
+//    below - setting up Bitmap (the drawing surface)
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    private void onTouch(float x, float y){
+        mPath = moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void moveTouch(float x, float y){
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+    }
 }
